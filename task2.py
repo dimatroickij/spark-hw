30d28c471722fb2aa3d110792f0c8ad4a3d00f90
dimatroickij <dimatroickij@yandex.ru>
2020-10-26 22:36:04 +0300

    edit task2, nearly task2_DF

--- task2.py
+++ task2.py
@@ -10,25 +10,19 @@ OUTPUT_FILE_LOCAL = '/home/dimatroickij/datasets/output'
 
 def getReviewMeta(jsonLine):
     decodeJsonLine = json.loads(jsonLine)
-    return (decodeJsonLine["asin"], decodeJsonLine["title"])
+    return decodeJsonLine["asin"], decodeJsonLine["title"]
 
 
 def getAvgRating(csvLine):
-    part = list()
-    for record in csvLine:
-        part.append(tuple(record.split(',')))
-    print(part)
-    return part
-#    decodeCsvLine = csvLine.split(',')
-    #decodeCsvLine = csv.reader(csvLine)
-#    response = []
-#    for value in decodeCsvLine:
-#        response.append(value)
-#    return response
+    prod_id, price = csvLine.split(',')
+    return prod_id, price
 
 
 def toCSVFile(data):
-    return ','.join([str(value) for value in data])
+    return data[0] + ',' + ','.join([str(value) for value in data[1]])
+    # print(type(data.split(',')[1]))
+    #    return data.split(',')[0] + '+' + data.split(',')[1]
+    # return ','.join([str(value) for value in data])
 
 
 conf = SparkConf().setAppName("task2")
@@ -37,8 +31,8 @@ rddReviewMeta = spark.textFile(INPUT_META_FILE_LOCAL)
 rddAvgRating = spark.textFile(INPUT_AVG_RATING_FILE_LOCAL)
 
 rddProdIdTitle = rddReviewMeta.map(lambda row: getReviewMeta(row))
-rddProdIdRating = rddAvgRating.mapPartitions(lambda row: getAvgRating(row)).collect()
+rddProdIdRating = rddAvgRating.map(lambda row: getAvgRating(row))
 
 result = rddProdIdTitle.join(rddProdIdRating, numPartitions=1)
 
-result.mapValues(toCSVFile).saveAsTextFile(OUTPUT_FILE_LOCAL)
+result.map(toCSVFile).saveAsTextFile(OUTPUT_FILE_LOCAL)
